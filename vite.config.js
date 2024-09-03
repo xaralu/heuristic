e71f2176ceeef1384be0051fa3a909b6e01911a5
xara <66925451+xaralu@users.noreply.github.com>
2024-09-03 09:52:11 -0400

    ngdyrrsfs

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
                 "./node_modules/three",
                 './three/examples/jsm/postprocessing/EffectComposer',
                 './three/examples/jsm/postprocessing/RenderPass',
-                'three/examples/jsm/postprocessing/GlitchPass',
-                'three/examples/jsm/postprocessing/RenderPixelatedPass',
-                'three/examples/jsm/postprocessing/AfterImagePass',
-                'three/examples/jsm/postprocessing/UnrealBloomPass',
-                'three/examples/jsm/postprocessing/OutputPass',
-                'three/examples/jsm/postprocessing/OutlinePass.js',
-                'three/examples/jsm/postprocessing/FilmPass.js'
+                './three/examples/jsm/postprocessing/GlitchPass',
+                './three/examples/jsm/postprocessing/RenderPixelatedPass',
+                './three/examples/jsm/postprocessing/AfterImagePass',
+                './three/examples/jsm/postprocessing/UnrealBloomPass',
+                './three/examples/jsm/postprocessing/OutputPass',
+                './three/examples/jsm/postprocessing/OutlinePass.js',
+                './three/examples/jsm/postprocessing/FilmPass.js'
             ]
         },
       },

f70a36ef9d2686036d43cf5df575e6d15657ad85
xara <66925451+xaralu@users.noreply.github.com>
2024-09-03 11:16:59 -0400

    dfkjgbfdsv

--- postprocessing.js
+++ postprocessing.js
@@ -1,12 +1,12 @@
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
-import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
+// import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
 import { RenderPixelatedPass } from 'three/examples/jsm/postprocessing/RenderPixelatedPass'
-import { AfterimagePass } from 'three/examples/jsm/postprocessing/AfterImagePass'
+// import { AfterimagePass } from '/three/examples/jsm/postprocessing/AfterImagePass'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass'
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js'
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass.js'
+// import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass.js'
 import { Vector2 } from 'three'
 
 
@@ -19,7 +19,7 @@ export function postprocessing(scene, camera, renderer) {
 	const renderPass = new RenderPass(scene, camera)
 	composer.addPass(renderPass)
 
-	const glitchPass = new GlitchPass()
+	// const glitchPass = new GlitchPass()
 
 	const outlinePass = new OutlinePass(
 		new Vector2(window.innerWidth, window.innerHeight),
@@ -40,9 +40,9 @@ export function postprocessing(scene, camera, renderer) {
 	pixelPass.normalEdgeStrength = 20 //change value to manipulate the strength of how well you can see the edges of shapes in the scene
 	//composer.addPass(pixelPass)
 
-	const afterPass = new AfterimagePass()
-	afterPass.uniforms.damp.value = 0.19
-	//composer.addPass(afterPass)
+	// const afterPass = new AfterimagePass()
+	// afterPass.uniforms.damp.value = 0.19
+	// //composer.addPass(afterPass)
 
 	const bloomPass = new UnrealBloomPass()
 	bloomPass.strength = 0.5
@@ -52,8 +52,8 @@ export function postprocessing(scene, camera, renderer) {
 	const outputPass = new OutputPass()
 	composer.addPass(outputPass)
 
-	const effectFilm = new FilmPass(0.15)
-	//composer.addPass(effectFilm)
+	// const effectFilm = new FilmPass(0.15)
+	// //composer.addPass(effectFilm)
 
 	return {
 		composer: composer,

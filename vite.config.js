d199872f5f5c661013a24f624072f241423f1058
xara <66925451+xaralu@users.noreply.github.com>
2024-09-03 09:51:19 -0400

    mhgjyf

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
         rollupOptions: {
             external: [
                 "./node_modules/three",
-                'three/examples/jsm/postprocessing/EffectComposer',
+                './three/examples/jsm/postprocessing/EffectComposer',
                 './three/examples/jsm/postprocessing/RenderPass',
                 'three/examples/jsm/postprocessing/GlitchPass',
                 'three/examples/jsm/postprocessing/RenderPixelatedPass',

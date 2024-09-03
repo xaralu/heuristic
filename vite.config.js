a6919a2143f3c33ca3236eb9b0c25612cbc4dd94
xara <66925451+xaralu@users.noreply.github.com>
2024-09-03 11:15:02 -0400

    sdfjvb

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
                 './three/examples/jsm/postprocessing/RenderPass',
                 './three/examples/jsm/postprocessing/GlitchPass',
                 './three/examples/jsm/postprocessing/RenderPixelatedPass',
-                './three/examples/jsm/postprocessing/AfterImagePass',
+                'three/examples/jsm/postprocessing/AfterImagePass',
                 './three/examples/jsm/postprocessing/UnrealBloomPass',
                 './three/examples/jsm/postprocessing/OutputPass',
                 './three/examples/jsm/postprocessing/OutlinePass.js',
